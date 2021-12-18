7141f341ba02786f3143b459a854a1c57a98b7b3
michalpirgl <michal.pirgl@gmail.com>
2021-12-18 12:50:35 +0100

    Very basic start/module to generate URL (#1)
    * Disable analytics in project
    * Very basic start/module to generate URL
    * Sonar cleanup
    
    Co-authored-by: Michal Pirgl <michal.pirgl@crackingbits.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalInviteEditorComponent } from './cal-invite-editor/cal-invite-editor.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalInviteEditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cal-invite-editor/cal-invite-editor.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+export const O365_BASE_URL = 'https://outlook.office.com/calendar/deeplink';
+
+@Component({
+  selector: 'app-cal-invite-editor',
+  templateUrl: './cal-invite-editor.component.html',
+  styleUrls: ['./cal-invite-editor.component.scss']
+})
+export class CalInviteEditorComponent implements OnInit {
+  generatedURL = '';
+  eventDetailsForm = new FormGroup({
+    subject: new FormControl(''),
+    body: new FormControl(''),
+    location: new FormControl(''),
+    startdt: new FormControl(''),
+    enddt: new FormControl(''),
+  });
+
+  ngOnInit(): void {
+    this.eventDetailsForm.patchValue({
+      subject: 'Title',
+      body: '<p>body<br> <br> URL example: <a href="https://www.google.com">Google</a></p>',
+      location: 'Location',
+      startdt: new Date().toISOString().substring(0, 16),
+      enddt: new Date(Date.now() + 60*60*1000).toISOString().substring(0, 16),
+    });
+  }
+
+  onSubmit() {
+    this.generatedURL = O365_BASE_URL
+        + "/compose"
+        + "?subject=" + encodeURIComponent(this.eventDetailsForm.value.subject)
+        + "&body=" + encodeURIComponent(this.eventDetailsForm.value.body)
+        + "&location=" + encodeURIComponent(this.eventDetailsForm.value.location)
+        + "&startdt=" + encodeURIComponent(this.eventDetailsForm.value.startdt)
+        + "&enddt=" + encodeURIComponent(this.eventDetailsForm.value.enddt);
+  }
+}
