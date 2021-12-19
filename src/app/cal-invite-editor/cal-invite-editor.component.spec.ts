1082729ee5ad5008c4b0a75c6eeea38c2c40ff49
michalpirgl <michal.pirgl@gmail.com>
2021-12-19 11:25:44 +0100

    Handle input from queryParams and enable shareUrl to persist work (#2)
    * Handle input from queryParams and enable shareUrl to persist work
    * Sonar fix for a href blank
    
    Co-authored-by: Michal Pirgl <michal.pirgl@crackingbits.com>

--- src/app/cal-invite-editor/cal-invite-editor.component.spec.ts
+++ src/app/cal-invite-editor/cal-invite-editor.component.spec.ts
@@ -1,12 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ControlContainer, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { Observable } from 'rxjs';
 import { CalInviteEditorComponent, O365_BASE_URL } from './cal-invite-editor.component';
+
+const DEFAULT_TEST_QUERY_PARAMS = {
+  subject: 'TEST SUBJECT IN QUERY PARAMS',
+  body: 'TEST QUERY PARAMS BODY <p>ščřžýáíé<a href="https://www.google.com">ĚŠČ</a>ŘŽÝÁÍÉ</p>',
+  location: 'TEST LOCATION IN QUERY PARAMS',
+  startdt: '2020-10-11T01:12',
+  enddt: '2020-10-11T14:00'
+};
 
 describe('CalInviteEditorComponent', () => {
   let component: CalInviteEditorComponent;
   let fixture: ComponentFixture<CalInviteEditorComponent>;
+
+  class ActivatedRouteMock {
+    queryParams = new Observable(observer => {
+       observer.next(DEFAULT_TEST_QUERY_PARAMS);
+       observer.complete();
+    });
+ }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,7 +30,14 @@ describe('CalInviteEditorComponent', () => {
         RouterTestingModule,
         ReactiveFormsModule
       ],
-      declarations: [ CalInviteEditorComponent ]
+      declarations: [ CalInviteEditorComponent ],
+      providers: [
+        ControlContainer,
+         {
+            provide: ActivatedRoute,
+            useClass: ActivatedRouteMock
+         }
+      ]
     })
     .compileComponents();
   }));
@@ -27,6 +50,14 @@ describe('CalInviteEditorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should read queryParams', () => {
+    expect(component.eventDetailsForm.value.subject).toEqual(DEFAULT_TEST_QUERY_PARAMS.subject);
+    expect(component.eventDetailsForm.value.body).toEqual(DEFAULT_TEST_QUERY_PARAMS.body);
+    expect(component.eventDetailsForm.value.location).toEqual(DEFAULT_TEST_QUERY_PARAMS.location);
+    expect(component.eventDetailsForm.value.startdt).toEqual(DEFAULT_TEST_QUERY_PARAMS.startdt);
+    expect(component.eventDetailsForm.value.enddt).toEqual(DEFAULT_TEST_QUERY_PARAMS.enddt);
   });
 
   it('should render input elements', () => {
@@ -61,5 +92,27 @@ describe('CalInviteEditorComponent', () => {
         + '&location=' + encodeURIComponent(component.eventDetailsForm.value.location)
         + '&startdt=' + encodeURIComponent(component.eventDetailsForm.value.startdt)
         + '&enddt=' + encodeURIComponent(component.eventDetailsForm.value.enddt));
+
+    expect(component.shareURL).toEqual('/'
+        + '?subject=' + encodeURIComponent(component.eventDetailsForm.value.subject)
+        + '&body=' + encodeURIComponent(component.eventDetailsForm.value.body)
+        + '&location=' + encodeURIComponent(component.eventDetailsForm.value.location)
+        + '&startdt=' + encodeURIComponent(component.eventDetailsForm.value.startdt)
+        + '&enddt=' + encodeURIComponent(component.eventDetailsForm.value.enddt));
+  });
+
+  it('sould erase string on reset', () => {
+    component.eventDetailsForm.value.subject = 'TEST SUBJECT';
+    component.eventDetailsForm.value.body = 'TEST BODY <p>ščřžýáíé<a href="https://www.google.com">ĚŠČ</a>ŘŽÝÁÍÉ</p>';
+    component.eventDetailsForm.value.location = 'TEST LOCATION';
+    component.eventDetailsForm.value.startdt = '2021-12-18T12:34';
+    component.eventDetailsForm.value.enddt = '2021-12-18T13:45';
+
+    component.onSubmit();
+
+    component.onResetForm();
+
+    expect(component.generatedURL).toEqual('');
+    expect(component.shareURL).toEqual('');
   });
 });
--- src/app/cal-invite-editor/cal-invite-editor.component.ts
+++ src/app/cal-invite-editor/cal-invite-editor.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 export const O365_BASE_URL = 'https://outlook.office.com/calendar/deeplink';
+export const DEFAULT_QUERY_PARAMS = {
+  subject: 'Title',
+  body: '<p>Body<br> <br> URL example: <a href="https://www.google.com">Google</a></p>',
+  location: 'Location',
+  startdt: new Date().toISOString().substring(0, 16),
+  enddt: new Date(Date.now() + 60 * 60 * 1000).toISOString().substring(0, 16)
+};
 
 @Component({
   selector: 'app-cal-invite-editor',
@@ -10,6 +18,7 @@ export const O365_BASE_URL = 'https://outlook.office.com/calendar/deeplink';
 })
 export class CalInviteEditorComponent implements OnInit {
   generatedURL = '';
+  shareURL = '';
   eventDetailsForm = new FormGroup({
     subject: new FormControl(''),
     body: new FormControl(''),
@@ -18,23 +27,45 @@ export class CalInviteEditorComponent implements OnInit {
     enddt: new FormControl(''),
   });
 
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
   ngOnInit(): void {
-    this.eventDetailsForm.patchValue({
-      subject: 'Title',
-      body: '<p>body<br> <br> URL example: <a href="https://www.google.com">Google</a></p>',
-      location: 'Location',
-      startdt: new Date().toISOString().substring(0, 16),
-      enddt: new Date(Date.now() + 60 * 60 * 1000).toISOString().substring(0, 16),
-    });
+    this.cleanURLs();
+    this.route.queryParams
+      .subscribe(params => {
+        this.eventDetailsForm.patchValue({
+          subject: params.subject ? params.subject : DEFAULT_QUERY_PARAMS.subject,
+          body: params.body ? params.body : DEFAULT_QUERY_PARAMS.body,
+          location: params.location ? params.location : DEFAULT_QUERY_PARAMS.location,
+          startdt: params.startdt ? params.startdt : DEFAULT_QUERY_PARAMS.startdt,
+          enddt: params.enddt ? params.enddt : DEFAULT_QUERY_PARAMS.enddt,
+        });
+      }
+    );
   }
 
   onSubmit() {
-    this.generatedURL = O365_BASE_URL
-        + '/compose'
-        + '?subject=' + encodeURIComponent(this.eventDetailsForm.value.subject)
-        + '&body=' + encodeURIComponent(this.eventDetailsForm.value.body)
-        + '&location=' + encodeURIComponent(this.eventDetailsForm.value.location)
-        + '&startdt=' + encodeURIComponent(this.eventDetailsForm.value.startdt)
-        + '&enddt=' + encodeURIComponent(this.eventDetailsForm.value.enddt);
+    this.prepareURLs();
+  }
+
+  onResetForm() {
+    this.cleanURLs();
+    this.router.navigate([]);
+  }
+
+  private prepareURLs() {
+    const queryParams = '?subject=' + encodeURIComponent(this.eventDetailsForm.value.subject)
+    + '&body=' + encodeURIComponent(this.eventDetailsForm.value.body)
+    + '&location=' + encodeURIComponent(this.eventDetailsForm.value.location)
+    + '&startdt=' + encodeURIComponent(this.eventDetailsForm.value.startdt)
+    + '&enddt=' + encodeURIComponent(this.eventDetailsForm.value.enddt);
+
+    this.generatedURL = O365_BASE_URL + '/compose' + queryParams;
+    this.shareURL = '/' + queryParams;
+  }
+
+  private cleanURLs() {
+    this.generatedURL = '';
+    this.shareURL = '';
   }
 }
