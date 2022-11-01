1cd6c4b13b4de1ed24c469cd984814590d34d33f
michalpirgl <michal.pirgl@gmail.com>
2022-11-01 18:11:05 +0100

    Fix CI (#21)
    * Karma coverage
    * CUSTOM_ELEMENTS_SCHEMA to app.spec
    * Update CLI versions
    * Update package lock
    
    Co-authored-by: Michal Pirgl <michal.pirgl@crackingbits.com>

--- karma.conf.js
+++ karma.conf.js
@@ -9,16 +9,28 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/crb-add-outlook-event'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/hackaference'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -33,8 +45,8 @@ module.exports = function (config) {
         statements: 80,
         branches: 80,
         functions: 80,
-        lines: 80
-      }
-    }
+        lines: 80,
+      },
+    },
   });
 };
